eeb1fc012b2abbca13e1f9d7dca7ad42a3b68696
Steve Doggett <steve@orbitalwitness.com>
2020-07-07 09:51:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# vue3-cinema
+## Build a Single-Page App with Vue Router & Vue CLI
+The is a course from [vuejsdevelopers.com](https://courses.vuejsdevelopers.com/courses/enrolled/979102).
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/main.js
@@ -0,0 +1,112 @@
+import { createApp } from "vue";
+
+import genres from "@/util/genres";
+
+import "@/assets/style.scss";
+
+createApp({
+  methods: {
+    checkFilter(checked, title) {
+      if (checked) {
+        this.genres.push(title);
+      } else {
+        this.genres = this.genres.filter(genre => genre !== title);
+      }
+      console.log(this.genres);
+    }
+  },
+  data: () => ({
+    genres: []
+  }),
+  components: {
+    "movie-list": {
+      props: {
+        genres: Array
+      },
+      template: `
+          <div id="movie-list">
+              <div v-for="movie in filteredMovies">{{ movie.title }}</div>
+          </div>
+      `,
+      data: () => ({
+        movies: [
+          {
+            title: "Pulp Fiction",
+            Genre: "Crime, Comedy"
+          },
+          {
+            title: "Home Alone",
+            Genre: "Comedy"
+          },
+          {
+            title: "Austin Powers",
+            Genre: "Comedy"
+          },
+          {
+            title: "Spirited Away",
+            Genre: "Animation"
+          }
+        ]
+      }),
+      computed: {
+        filteredMovies() {
+          return this.movies.filter(movie => {
+            if (!this.genres.length) {
+              return true;
+            } else {
+              const movieGenres = movie.Genre.split(", ");
+              return movieGenres.some(genre => this.genres.includes(genre));
+            }
+          });
+        }
+      }
+    },
+    "movie-filter": {
+      emits: ["check-filter"],
+      template: `
+          <div id="movie-filter">
+              <h2>Filter results</h2>
+              <div class="filter-group">
+                  <check-filter v-for="genre in genres"
+                                v-bind:title="genre"
+                                v-on:check-filter="checkFilter"></check-filter>
+              </div>
+          </div>
+      `,
+      data: () => ({
+        genres
+      }),
+      methods: {
+        checkFilter(checked, title) {
+          this.$emit("check-filter", checked, title);
+        }
+      },
+      components: {
+        "check-filter": {
+          props: {
+            title: String
+          },
+          emits: ["check-filter"],
+          data: () => ({
+            checked: false
+          }),
+          methods: {
+            checkFilter() {
+              this.checked = !this.checked;
+              this.$emit("check-filter", this.checked, this.title);
+            }
+          },
+          // eslint-disable-next-line
+          template: `
+              <div v-on:click="checkFilter"
+                   v-bind:class="{ 'check-filter': true, active: checked }"
+              >
+                  <span class="checkbox"></span>
+                  <span class="check-filter-title">{{ title }}</span>
+              </div>
+          `
+        }
+      }
+    }
+  }
+}).mount("#app");
